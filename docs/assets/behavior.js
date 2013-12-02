5f982d87935e177636f2c2fb11ef94a6bc5a100a
Scott Nonnenberg <scott@nonnenberg.com>
2013-12-02 13:06:35 -0800

    doc catchup: index/grunt_config

--- docs/assets/behavior.js
+++ docs/assets/behavior.js
@@ -75,6 +75,14 @@ f.event={add:function(a,c,d,e,g){var h,i,j,k,l,m,n,o,p,q,r,s;if(!(a.nodeType===3
                   "type": "heading",
                   "data": {
                     "level": 3,
+                    "title": "2.0.0",
+                    "slug": "200"
+                  },
+                  "depth": 3
+                }, {
+                  "type": "heading",
+                  "data": {
+                    "level": 3,
                     "title": "1.4.1",
                     "slug": "141"
                   },
@@ -229,6 +237,14 @@ f.event={add:function(a,c,d,e,g){var h,i,j,k,l,m,n,o,p,q,r,s;if(!(a.nodeType===3
           "depth": 2,
           "children": [
             {
+              "type": "heading",
+              "data": {
+                "level": 3,
+                "title": "2.0.0",
+                "slug": "200"
+              },
+              "depth": 3
+            }, {
               "type": "heading",
               "data": {
                 "level": 3,
