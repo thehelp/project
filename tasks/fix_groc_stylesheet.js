01274c451b99815ed5d70e845970e2962a8fcda0
Scott Nonnenberg <scott@nonnenberg.com>
2014-06-22 17:00:39 -0700

    Remove docs directory from repo

--- tasks/fix_groc_stylesheet.js
+++ tasks/fix_groc_stylesheet.js
@@ -5,11 +5,13 @@
 
 var path = require('path');
 
+// You can supply your own `source` on the grunt configuration for this task and it will
+// override the default supplied here.
 module.exports = function(grunt) {
   grunt.registerTask('fix-groc-stylesheet', 'Replace generated groc style sheet.',
     function() {
       var options = this.options({
-        source: path.join(__dirname, '../docs/style.css'),
+        source: path.join(__dirname, '../src/style.css'),
         target: 'docs/assets/style.css'
       });
 
